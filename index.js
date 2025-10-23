d85f1f3e589a248b0e3d0bb58bcd5d1a2c64b158
Jack Collins <jackc@cybcube.com>
2025-10-23 13:43:45 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require ('express');
+const app = express(); //calling this like a function represents a new express app.  Can have multiple in a node project, but rare
+
+app.get('/', (req, res)=>{ //app. is saying make me a route handler.  get says "what type (post, put, delete, etc)"  / is the path to listen for. req and res are request and response
+    res.send({hi: 'there'});
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
