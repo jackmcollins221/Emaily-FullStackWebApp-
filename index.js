21aa41b42a5fb592e8d65b994cb8483c0ebd252c
Jack Collins <jackc@cybcube.com>
2025-10-29 17:56:15 -0500

    no keys

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+
+
 const express = require ('express');
+const passport = require ('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express(); //calling this like a function represents a new express app.  Can have multiple in a node project, but rare
 
-app.get('/', (req, res)=>{ //app. is saying make me a route handler.  get says "what type (post, put, delete, etc)"  / is the path to listen for. req and res are request and response
-    res.send({Trying_Again: 'Max Payne'});
-});
+passport.use(new GoogleStrategy());
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
